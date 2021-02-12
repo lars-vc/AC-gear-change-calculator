4bed6ec8061bc545428626838cf29e44d5736858
Lars Van Cauter <larsvancauter@gmail.com>
2021-02-12 15:05:33 +0100

    first

---
+++ racecalc.py
@@ -0,0 +1,67 @@
+import math
+import matplotlib.pyplot as plt
+
+
+power = []
+with open("power.lut", "r") as file:
+    line = file.readline()
+    while line:
+        s = line.split("|")
+        s = s[0:len(s)]
+        power.append((int(s[0]), int(s[1])))
+        line = file.readline()
+
+gear = [
+    2.5, 1.875, 1.529, 1.2777, 1.105, 1
+]
+final = 3.6
+radius = 0.32
+ratios = []
+for i in gear:
+    ratios.append((((1000/60)/i)/final)*radius*2*math.pi*3.6)
+
+table = []
+for ratio in ratios:
+    val = []
+    for p in power:
+        val.append((p[0]*ratio/1000, p[1]/ratio))
+    table.append(val)
+
+speeds = []
+for i, g in enumerate(table):
+    for j, tup in enumerate(g):
+        if j+1 < len(g) and i+1 < len(table):
+            nexttup = g[j+1]
+            nextgear = table[i+1]
+            for count,x in enumerate(nextgear):
+                if round(tup[0],2) <= x[0] <= round(nexttup[0],2):
+                    # check torque
+                    if round(tup[1],2) > round(nexttup[1],2) and round(nexttup[1],2) <= round(x[1],2) <= round(tup[1],2):
+                        prev = nextgear[count-1]
+                        #next gear points
+                        x1,y1 = x
+                        x2,y2 = prev
+                        m1 = (y2-y1)/(x2-x1)
+                        b1 = y1 - m1*x1
+
+                        #curr gear points
+                        x1,y1 = tup
+                        x2,y2 = nexttup
+                        m2 = (y2-y1)/(x2-x1)
+                        b2 = y1 - m2*x1
+
+                        #combine
+                        total = (b1-b2)/(m2-m1)
+                        print(f"found {x}")
+                        print(f"calculated: {total}")
+                        speeds.append(total)
+
+
+
+print(power)
+print(ratios)
+print(table)
+print(speeds)
+
+#plt.plot(*zip(*table[0]))
+#plt.show()
